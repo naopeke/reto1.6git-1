78b0bbfbbc3ac0002af148e6c7a5047c05e3dc5f
naopeke <naopeke@protonmail.com>
2023-10-24 13:20:49 +0200

    first commit  added a function of square

---
+++ index.js
@@ -0,0 +1,26 @@
+
+function suma(x, y){
+    return x + y;
+    };
+    
+    function resta(x, y){
+        return x - y;
+    }
+    
+    function producto(x, y){
+        return x * y;
+    }
+    
+    function division(x, y){
+        return x / y;
+    }
+
+    function cuadrado(x){
+        return x * x;
+    }
+    
+    
+    module.exports = {
+        suma, resta, producto, division
+    };
+    
